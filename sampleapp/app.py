e6fa5ccfd1bf0d15719c40d8be93253ba1a957c2
Theron Luhn <theron@luhn.com>
2020-01-01 15:28:36 -0800

    Add sample application.

---
+++ sampleapp/app.py
@@ -0,0 +1,6 @@
+
+def application(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+    start_response(status, headers)
+    return [b"Hello Gunicorn!\n"]
